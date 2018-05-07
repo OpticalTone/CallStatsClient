a2df65a7e62ddf1dbe6d999a9d86f54c4e4041bb
ivanab <ivana@opticaltone.com>
2018-05-07 18:22:36 +0200

    Added MediaPlayback method

--- CallStatsClient/RestClient.cs
+++ CallStatsClient/RestClient.cs
@@ -811,6 +811,24 @@ namespace CallStatsClient
             await SendRequest(data, url);
         }
 
+        public async Task MediaPlayback()
+        {
+            string url = $"https://events.callstats.io/v1/apps/{appID}/conferences/{confID}/{ucID}/events/media/pipeline";
+
+            object data = new
+            {
+                eventType = "mediaPlaybackStart",
+                localID = localID,
+                originID = "originID",
+                deviceID = "deviceID",
+                timestamp = TimeStamp.Now(),
+                mediaType = "mediaType",
+                ssrc = "11"
+            };
+
+            await SendRequest(data, url);
+        }
+
         #endregion
 
         #region Special Events
