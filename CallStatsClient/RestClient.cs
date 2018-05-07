1c430c4bd4d620a0f1df615eb6572bc4cb5adb17
ivanab <ivana@opticaltone.com>
2018-05-07 18:08:50 +0200

    Added IceConnectionDisruptionEnd method

--- CallStatsClient/RestClient.cs
+++ CallStatsClient/RestClient.cs
@@ -741,7 +741,7 @@ namespace CallStatsClient
             await SendRequest(data, url);
         }
 
-        public async Task ConnectionDisruptionStart()
+        public async Task IceConnectionDisruptionStart()
         {
             string url = $"https://events.callstats.io/v1/apps/{appID}/conferences/{confID}/{ucID}/events/ice/status";
 
@@ -756,6 +756,27 @@ namespace CallStatsClient
                 connectionID = ucID,
                 currIceConnectionState = "disconnected",
                 prevIceConnectionState = "checking"
+            };
+
+            await SendRequest(data, url);
+        }
+
+        public async Task IceConnectionDisruptionEnd()
+        {
+            string url = $"https://events.callstats.io/v1/apps/{appID}/conferences/{confID}/{ucID}/events/ice/status";
+
+            object data = new
+            {
+                eventType = "iceDisruptionStart",
+                localID = localID,
+                originID = "originID",
+                deviceID = "deviceID",
+                timestamp = TimeStamp.Now(),
+                remoteID = "remoteID",
+                connectionID = ucID,
+                currIceConnectionState = "checking",
+                prevIceConnectionState = "disconnected",
+                delay = 0
             };
 
             await SendRequest(data, url);
